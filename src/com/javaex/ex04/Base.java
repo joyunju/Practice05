c2f22c670fde055ed2e5f91cd88fe6a69e5025df
joyunju <whdbswn1113@naver.com>
2022-04-26 01:37:09 +0900

    1.2

--- src/com/javaex/ex04/Base.java
+++ src/com/javaex/ex04/Base.java
@@ -1,23 +1,29 @@
 package com.javaex.ex04;
 
 public class Base {
-    
-    public void service(String state) {
-        
-        //코드작성할것
-    }
 
-    public void day() {
-        System.out.println("낮에는 열심히 수업듣자");
-    }
+	// 필드
+	private String service;
 
-    public void night() {
-        
-    	//코드작성할것
-    }
-    
-    public void afternoon(){
-        System.out.println("오후도 낮과 마찬가지로 공부해야 합니다.");
-    }
+	// 생성자
+	public void service(String state) {
+		// 코드작성할것
+		
+	}
+
+	// 메소드 - gs
+	
+	// 메소드 - 일반
+	public void day() {
+		System.out.println("낮에는 열심히 수업듣자");
+	}
+
+	public void night() {
+		System.out.println("밤에는 숙면");
+		// 코드작성할것
+	}
+
+	public void afternoon() {
+		System.out.println("오후도 낮과 마찬가지로 공부해야 합니다.");
+	}
 }
-
