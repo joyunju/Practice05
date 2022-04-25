e754e76659e257009b113fdbcc58b4013191b7b2
joyunju <whdbswn1113@naver.com>
2022-04-26 01:09:00 +0900

    오류이유 : 디폴트 생성자

--- src/com/javaex/ex02/GoodsApp.java
+++ src/com/javaex/ex02/GoodsApp.java
@@ -1,12 +1,19 @@
+//GoodsApp 클래스는 수정하지 말 것!
+
 package com.javaex.ex02;
 
 public class GoodsApp {
 
 	public static void main(String[] args) {
 
-		
+		Goods camera = new Goods();
+		Goods cup = new Goods("머그컵", 2000);
+
+		camera.setName("니콘");
+		camera.setPrice(400000);
+
+		camera.showInfo();
+		cup.showInfo();
 	}
 
 }
-
-
