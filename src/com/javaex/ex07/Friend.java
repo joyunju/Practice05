59885cbc629c1aaab5ac2ecdb54c70c67943d63d
joyunju <whdbswn1113@naver.com>
2022-04-27 22:46:07 +0900

    배열 / nextLine() / next()

--- src/com/javaex/ex07/Friend.java
+++ src/com/javaex/ex07/Friend.java
@@ -2,17 +2,51 @@ package com.javaex.ex07;
 
 public class Friend {
 
-    private String name;
-    private String hp;
-    private String school;
+	// 필드
+	private String name;
+	private String hp;
+	private String school;
 
-    
-    // getter/setter 작성
-    
-    
-    
-    public void showInfo(){
-        System.out.println("이름:"+name+"  핸드폰:"+hp+"  학교:"+school);
-    }
+	// 생성자
+	public Friend() {
+		// default
+	}
+
+	public Friend(String name, String hp, String school) {
+		super();
+		this.name = name;
+		this.hp = hp;
+		this.school = school;
+	}
+
+	// 메소드 - getter/setter 작성
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getHp() {
+		return hp;
+	}
+
+	public void setHp(String hp) {
+		this.hp = hp;
+	}
+
+	public String getSchool() {
+		return school;
+	}
+
+	public void setSchool(String school) {
+		this.school = school;
+	}
+
+	// 메소드 일반
+	public void showInfo() {
+		System.out.println("이름:" + name + "  핸드폰:" + hp + "  학교:" + school);
+	}
 
 }
--- src/com/javaex/ex07/FriendApp.java
+++ src/com/javaex/ex07/FriendApp.java
@@ -1,27 +1,55 @@
+// 1. java.util.Scanner 클래스를 이용하여 친구정보를 입력받아 Friend 객체를 생성
+// 2. 이들을 Friend 객체 배열에 저장 
+// 3. 친구 즉, 친구(Friend)객체를 3개 입력 받으면 이들을 모두 화면에 출력하세요.
+
 package com.javaex.ex07;
 
 import java.util.Scanner;
 
 public class FriendApp {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        Friend[] friendArray = new Friend[3];
-        Scanner sc = new Scanner(System.in);
+		Friend[] friendArray = new Friend[3];
 
-        System.out.println("친구를 3명 등록해 주세요");
+		Scanner sc = new Scanner(System.in);
 
-        //친구정보 3명 입력 로직 --> 반복문 사용
-            
-        
+		System.out.println("친구를 3명 등록해 주세요");
 
-        
-        // 친구정보 출력
-        for (int i = 0; i < friendArray.length; i++) {
-            //친구정보 출력 메소드 호출
-        }
+		// 친구정보 3명 입력 로직 --> 반복문 사용
+		for (int i = 0; i < friendArray.length; i++) {
 
-        sc.close();
-    }
+			Friend friend = new Friend(); // Friend 클래스
+
+			System.out.print("이름 : ");
+			String name = sc.nextLine();
+			friend.setName(name);
+			// next() 는 문자 혹은 문자열을 공백 기준으로 한 단어 또는 한 문자씩 입력을 받는다.
+			// nextLine()의 경우에는 문자 혹은 엔터를 치기 전까지의 문장 전체를 입력받는다.
+
+			System.out.print("핸드폰 : ");
+			friend.setHp(sc.nextLine());
+
+			System.out.print("학교 : ");
+			friend.setSchool(sc.nextLine());
+
+			System.out.println("-----------------");
+
+			friendArray[i] = friend; // 배열에 저장
+
+		}
+
+		// 친구정보 출력
+		for (int i = 0; i < friendArray.length; i++) {
+			// 친구정보 출력 메소드 호출
+			friendArray[i].showInfo();
+
+			// friendArray[0].showInfo();
+			// friendArray[1].showInfo();
+			// friendArray[2].showInfo();
+		}
+
+		sc.close();
+	}
 
 }
