1c0b127fe63d0b99634c93ff9d1eeebf9ba9d145
joyunju <whdbswn1113@naver.com>
2022-04-28 17:37:41 +0900

    static 필드와 메소드

--- src/com/javaex/ex06/CConverterApp.java
+++ src/com/javaex/ex06/CConverterApp.java
@@ -1,21 +1,23 @@
 package com.javaex.ex06;
 
-
 public class CConverterApp {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        double dollar;
-        double won;
-        
-        CConverter.setRate(1118.70);
-        
-        //100만원을 달러로 출력하기
+		double dollar;
+		double won;
 
-        
-        //100달려를 원으로 출력하기
-        
-        
-    }
+		CConverter.setRate(1118.70);
+
+		// 100만원을 달러로 출력하기
+		won = 1000000;
+		System.out.println("백만원은 " + CConverter.toDoller(won) + "달러입니다.");
+		// CConverter.toDoller(1000000);
+
+		// 100달려를 원으로 출력하기
+		dollar = 100;
+		System.out.println("백달러는 " + CConverter.toKWR(dollar) + "입니다.");
+
+	}
 
 }
