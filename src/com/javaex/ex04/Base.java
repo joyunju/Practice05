c2822a7fe318d6bd0648a44ca97df16ebbdcd1dc
joyunju <whdbswn1113@naver.com>
2022-05-07 11:04:17 +0900

    수정

--- src/com/javaex/ex04/Base.java
+++ src/com/javaex/ex04/Base.java
@@ -8,11 +8,20 @@ public class Base {
 	// 생성자
 	public void service(String state) {
 		// 코드작성할것
-		
+		this.service = state;
+
+		if (state == "낮") {
+			this.day();
+		} else if (state.equals("오후")) {
+			this.afternoon();
+		} else if (state.equals("밤")) {
+			this.night();
+		}
+
 	}
 
 	// 메소드 - gs
-	
+
 	// 메소드 - 일반
 	public void day() {
 		System.out.println("낮에는 열심히 수업듣자");
@@ -21,6 +30,7 @@ public class Base {
 	public void night() {
 		System.out.println("밤에는 숙면");
 		// 코드작성할것
+
 	}
 
 	public void afternoon() {
